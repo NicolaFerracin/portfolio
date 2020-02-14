8b32a5442702596da2b3f78187cb5ed25ad26058
Nicola Ferracin <nicola.ferracin@gmail.com>
2020-02-14 18:24:13 +0100

    Update portfolio

--- public/js/portfolio.js
+++ public/js/portfolio.js
@@ -1,58 +1,18 @@
 (function() {
-    function Portfolio() {
-        this.openInfoTriggers = document.querySelectorAll('.projects .project .project__info-open');
-        this.closeInfoTriggers = document.querySelectorAll('.projects .project .project__info-close');
-        this.addEventListeners();
-    }
+  var accordions = document.querySelectorAll('.toggle');
+  accordions.forEach(a => a.addEventListener('click', toggleAccordion));
+}())
 
-    Portfolio.fn = Portfolio.prototype;
-
-    Portfolio.fn.addEventListeners = function() {
-        this.openInfoTriggers.forEach(
-            trigger => trigger.addEventListener(
-                'click', this.onOpenInfoClick.bind(this)
-            )
-        );
-        this.closeInfoTriggers.forEach(
-            trigger => trigger.addEventListener(
-                'click', this.onCloseInfoClick.bind(this)
-            )
-        );
-    }
-
-    Portfolio.fn.onOpenInfoClick = function(e) {
-        const infoEl = document.getElementById(e.currentTarget.dataset.trigger);
-        this.openInfo(infoEl);
-    }
-
-    Portfolio.fn.onCloseInfoClick = function(e) {
-        const infoEl = e.currentTarget.parentElement;
-        this.closeInfo(infoEl);
-    }
-
-    Portfolio.fn.openInfo = function(infoEl) {
-        const previewEl = infoEl.previousElementSibling;
-        const projectEl = infoEl.parentElement;
-        previewEl.style.transform = `translateX(-${previewEl.offsetWidth}px)`;
-        previewEl.style.visibility = 'hidden';
-        infoEl.style.transform = `translateX(0px)`;
-        infoEl.style.visibility = 'visible';
-        if (infoEl.scrollHeight > projectEl.offsetHeight) {
-            projectEl.style.height = `${infoEl.scrollHeight}px`;
-        }
-    }
-
-    Portfolio.fn.closeInfo = function(infoEl) {
-        const previewEl = infoEl.previousElementSibling;
-        previewEl.style.visibility = 'visible';        
-        previewEl.style.transform = null;
-        const projectEl = infoEl.parentElement;
-        projectEl.style.height = '200px';
-        infoEl.style.transform = `translateX(${previewEl.offsetWidth}px)`;
-        infoEl.style.visibility = 'hidden';
-    }
-
-    window.addEventListener('DOMContentLoaded', function() {
-        const portfolio = new Portfolio();
-    });
-}());
+function toggleAccordion(e) {
+  var accordion = e.currentTarget;
+  var isActive = accordion.classList.contains('active');
+  var content = e.currentTarget.nextElementSibling;
+  if (isActive) {
+    accordion.classList.remove('active');
+    accordion.classList.add('inactive');
+  } else {
+    accordion.classList.remove('inactive');
+    accordion.classList .add('active');
+  }
+  content.classList.toggle('hidden')
+}
